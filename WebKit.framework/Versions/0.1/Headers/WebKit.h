d95193866d998fec91dd67534b059eda328f79ed
Gregory John Casamento <greg.casamento@gmail.com>
2025-06-02 13:01:10 -0400

    Add initial files

---
+++ WebKit.framework/Versions/0.1/Headers/WebKit.h
@@ -0,0 +1,38 @@
+/*
+   Project: WebKit
+
+   Copyright (C) 2025 Free Software Foundation
+
+   Author: Gregory John Casamento,,,
+
+   Created: 2025-06-02 12:58:35 -0400 by heron
+
+   This application is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This application is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 31 Milk Street #960789 Boston, MA 02196 USA.
+*/
+
+#ifndef _WEBKIT_H_
+#define _WEBKIT_H_
+
+#import <Foundation/Foundation.h>
+
+@interface WebKit : NSObject
+{
+
+}
+
+@end
+
+#endif // _WEBKIT_H_
+
