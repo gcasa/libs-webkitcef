4e9633edce15abe9005045cfa258090bb1c8f331
Gregory John Casamento <greg.casamento@gmail.com>
2025-07-14 07:26:58 -0400

    Move things into new configuration

---
+++ Applications/WebBrowser/AppController.h
@@ -0,0 +1,43 @@
+/* 
+   Project: WebBrowser
+
+   Author: Gregory John Casamento,,,
+
+   Created: 2025-07-14 07:25:29 -0400 by heron
+   
+   Application Controller
+*/
+ 
+#ifndef _PCAPPPROJ_APPCONTROLLER_H
+#define _PCAPPPROJ_APPCONTROLLER_H
+
+#import <AppKit/AppKit.h>
+// Uncomment if your application is Renaissance-based
+//#import <Renaissance/Renaissance.h>
+
+@interface AppController : NSObject
+{
+  IBOutlet NSWindow *_window;
+}
+
+// Class methods...
++ (void)  initialize;
+
+// Initialization
+- (id) init;
+- (void) dealloc;
+- (void) awakeFromNib;
+
+// Notification methods...
+- (void) applicationDidFinishLaunching: (NSNotification *)aNotif;
+- (BOOL) applicationShouldTerminate: (id)sender;
+- (void) applicationWillTerminate: (NSNotification *)aNotif;
+- (BOOL) application: (NSApplication *)application
+	    openFile: (NSString *)fileName;
+
+// Actions...
+- (IBAction) showPrefPanel: (id)sender;
+
+@end
+
+#endif
